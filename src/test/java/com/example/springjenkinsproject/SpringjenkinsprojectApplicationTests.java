d4a4660301305693f2790036e3b45163c6d214ee
hemalatp <hemalatha.p@global-csg.com>
2023-03-20 18:12:42 +0530

    first commit

---
+++ src/test/java/com/example/springjenkinsproject/SpringjenkinsprojectApplicationTests.java
@@ -0,0 +1,20 @@
+package com.example.springjenkinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringjenkinsprojectApplicationTests {
+	//to check if the test process is auotmated or not with the help of jenkins
+	public static Logger logger = LoggerFactory.getLogger(SpringjenkinsprojectApplication.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing........");
+		assertEquals(true, true);
+	}
+}
