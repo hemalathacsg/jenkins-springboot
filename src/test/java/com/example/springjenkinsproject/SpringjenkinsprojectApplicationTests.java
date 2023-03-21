97897bcea287cfc1c132f48fb28ffee228030e84
hemalatp <hemalatha.p@global-csg.com>
2023-03-21 09:28:42 +0530

    second commit

--- src/test/java/com/example/springjenkinsproject/SpringjenkinsprojectApplicationTests.java
+++ src/test/java/com/example/springjenkinsproject/SpringjenkinsprojectApplicationTests.java
@@ -15,6 +15,7 @@ class SpringjenkinsprojectApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing........");
+		logger.info("Test case executing second log statement  ........");
 		assertEquals(true, true);
 	}
 }
