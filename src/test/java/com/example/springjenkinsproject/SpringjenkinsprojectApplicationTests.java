04eddca1cbd2876b1dba318fba311f42ff486c70
hemalatp <hemalatha.p@global-csg.com>
2023-03-21 16:56:17 +0530

    tenth commit

--- src/test/java/com/example/springjenkinsproject/SpringjenkinsprojectApplicationTests.java
+++ src/test/java/com/example/springjenkinsproject/SpringjenkinsprojectApplicationTests.java
@@ -14,8 +14,8 @@ class SpringjenkinsprojectApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		logger.info("Test case executing........");
-		logger.info("Test case executing second log statement  ........");
+//		logger.info("Test case executing........");
+//		logger.info("Test case executing second log statement  ........");
 		assertEquals(true, true);
 	}
 }
